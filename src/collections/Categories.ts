a22a9ace8e4a928de630f7b8c33c583a9ae94286
JairGuzman1810 <jair18101@hotmail.com>
2025-04-15 21:50:26 -0600

    feat: add Categories collection configuration and payload types

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,13 +1,10 @@
 import type { CollectionConfig } from "payload";
 
-export const Media: CollectionConfig = {
-  slug: "media",
-  access: {
-    read: () => true,
-  },
+export const Categories: CollectionConfig = {
+  slug: "categories",
   fields: [
     {
-      name: "alt",
+      name: "name",
       type: "text",
       required: true,
     },
